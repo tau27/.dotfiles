188bc864a9d66d235136b95ac81b624ef4423c2a
djamalArch <lazey07@proton.me>
2022-09-15 23:11:21 +0300

    Itit

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias pls=sudo
+alias pswrd="cat ~/Common/file.txt"
+starship init fish | source
+set PATH $PATH:/home/djamal/.cargo/bin
+export LANG=en_US.UTF-8
+export LC_CTYPE="ru_RU.UTF-8"
