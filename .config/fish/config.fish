eaf31ebc1a47f5adce82449e83e2dc1c8696e80f
Gleb <lazey07@proton.me>
2022-11-07 21:55:02 +0300

    Upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,10 +7,14 @@ alias pls="sudo "
 alias pswrd="cat ~/Sync/Common/file.txt"
 
 starship init fish | source
-set PATH $PATH:/home/djamal/.cargo/bin
 thefuck --alias | source
+
+set PATH $PATH:/home/djamal/.cargo/bin
+set DOTNET_CLI_TELEMETRY_OPTOUT 1
+
 export LANG=en_US.UTF-8
 export LC_CTYPE="ru_RU.UTF-8"
+
 set PATH $( python -c "
 import os
 path = os.environ['PATH'].split(':')
