24d1e2211661f8c28c227599361fd183d915329f
Gleb <lazey07@proton.me>
2022-12-08 21:36:21 +0300

    Upd

---
+++ .config/hypr/swapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mv ~/.config/hypr/hyprland.conf ~/.config/hypr/other1land.conf
+mv ~/.config/hypr/otherland.conf ~/.config/hypr/hyprland.conf
+mv ~/.config/hypr/other1land.conf ~/.config/hypr/otherland.conf
