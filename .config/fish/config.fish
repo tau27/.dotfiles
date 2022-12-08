c054b198c5ae6d26166b6fb2993042fa02898490
Gleb <lazey07@proton.me>
2022-12-08 21:18:55 +0300

    Upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,11 +5,14 @@ end
 alias la="ls -la"
 alias pls="sudo "
 alias pswrd="cat ~/Sync/Common/file.txt"
+alias pick=~/.config/fish/picker.sh
 
 starship init fish | source
 thefuck --alias | source
 
 set PATH $PATH:/home/djamal/.cargo/bin
+set PATH $PATH:/home/gleb/.local/bin
+set PATH $PATH:/home/gleb/bin
 set DOTNET_CLI_TELEMETRY_OPTOUT 1
 
 export LANG=en_US.UTF-8
@@ -22,3 +25,4 @@ print(':'.join(sorted(set(path), key=path.index)))
 " )
 
 export QT_QPA_PLATFORMTHEME=qt5ct
+export BAT_THEME="Nord"
